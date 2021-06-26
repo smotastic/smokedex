d7b0645f766a1ab55851f635facf76033f1b2523
Nils Smolinski <nilssmol@gmail.com>
2021-06-26 18:20:44 +0200

    Show Pokemon Types

--- smokedex/lib/features/pokedex/presentation/widgets/pokemon_list_card.dart
+++ smokedex/lib/features/pokedex/presentation/widgets/pokemon_list_card.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:smokedex/features/pokedex/domain/entities/pokemon_entry.dart';
 
 class PokemonListCard extends StatelessWidget {
   final PokemonEntry entry;
 
   const PokemonListCard(this.entry, {Key? key}) : super(key: key);
+
+  static final Map<String, Color> _typeColors = Map.unmodifiable({
+    'bug': Color.fromRGBO(146, 188, 44, 1.0),
+    'dark': Color.fromRGBO(89, 87, 97, 1.0),
+    'dragon': Color.fromRGBO(12, 105, 200, 1.0),
+    'electric': Color.fromRGBO(242, 217, 78, 1.0),
+    'fairy': Color.fromRGBO(238, 144, 230, 1.0),
+    'fighting': Color.fromRGBO(211, 66, 95, 1.0),
+    'fire': Color.fromRGBO(251, 165, 76, 1.0),
+    'flying': Color.fromRGBO(161, 187, 236, 1.0),
+    'ghost': Color.fromRGBO(95, 109, 188, 1.0),
+    'grass': Color.fromRGBO(95, 189, 88, 1.0),
+    'ground': Color.fromRGBO(218, 124, 77, 1.0),
+    'ice': Color.fromRGBO(117, 208, 193, 1.0),
+    'normal': Color.fromRGBO(160, 162, 159, 1.0),
+    'poison': Color.fromRGBO(183, 99, 207, 1.0),
+    'psychic': Color.fromRGBO(250, 133, 129, 1.0),
+    'rock': Color.fromRGBO(201, 187, 138, 1.0),
+    'steel': Color.fromRGBO(86, 149, 163, 1.0),
+    'water': Color.fromRGBO(83, 157, 223, 1.0),
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -78,24 +100,37 @@ class PokemonListCard extends StatelessWidget {
                       Text('#${entry.id}'),
                     ],
                   ),
+                  // TYPES
                   Row(
-                    children: [
-                      Icon(Icons.work),
-                      SizedBox(
-                        width: 6,
-                      ),
-                      Text(entry.types.toString()),
-                    ],
+                    children: entry.types.map((type) {
+                      return CircleAvatar(
+                        backgroundColor: _typeColors[type],
+                        child: SvgPicture.asset(
+                          'assets/icons/$type.svg',
+                          width: 18,
+                          height: 18,
+                        ),
+                      );
+                    }).toList(),
                   ),
-                  Row(
-                    children: [
-                      Icon(Icons.attach_money),
-                      SizedBox(
-                        width: 6,
-                      ),
-                      Text('Afford'),
-                    ],
-                  )
+                  // Row(
+                  //   children: [
+                  //     Icon(Icons.work),
+                  //     SizedBox(
+                  //       width: 6,
+                  //     ),
+                  //     Text(entry.types.toString()),
+                  //   ],
+                  // ),
+                  // Row(
+                  //   children: [
+                  //     Icon(Icons.attach_money),
+                  //     SizedBox(
+                  //       width: 6,
+                  //     ),
+                  //     Text('Afford'),
+                  //   ],
+                  // )
                 ],
               ),
             ),
